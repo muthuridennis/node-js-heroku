f442ef3862a474c91de3fba0d7bed0cba4d09bd5
dmuthuri <muthuridennis@gmail.com>
2015-11-21 21:47:12 +0300

    Look for the port defined in the environment variable before using the default one

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var PORT = 9000;
 
+app.set('port', (process.env.PORT || PORT));
+
 app.get('/', function(req, res){
 	res.send('This is my Node app and it works on Heroku');
 });
 
-app.listen(PORT, function(){
-	console.log('Server running on PORT, %s', PORT);
+app.listen(app.get('port'), function(){
+	console.log('Server running on PORT, %s', app.get('port'));
 });
